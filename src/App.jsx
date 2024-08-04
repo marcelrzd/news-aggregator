f8e571a071371e6e5109975e561e848c92d4cd45
Marcel <mrezende@bcfsolutions.net.br>
2024-08-04 11:37:01 -0300

    fix: changes in folder structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import Header from "./components/Header";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="bg-red-400 h-screen">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="p-4 font-semibold bg-slate-300 text-black">
+      <h1>News Aggregator</h1>
+    </header>
+  );
+};
+
+export default Header;
